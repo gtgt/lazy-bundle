abc738c3dbfc7ba4cd4173d10669d78c6014da0f
GT <788449+gtgt@users.noreply.github.com>
2020-03-31 04:51:09 +0200

    fixes T40 WebDav storage backend - EntityListener hints

--- src/LazyBundle/EntityListener/EntityListenerInterface.php
+++ src/LazyBundle/EntityListener/EntityListenerInterface.php
@@ -1,7 +1,30 @@
 <?php
+
 namespace LazyBundle\EntityListener;
 
 
 interface EntityListenerInterface {
+    /** @ORM\PrePersist */
+    //public function prePersistHandler(User $user, LifecycleEventArgs $event);
 
+    /** @ORM\PostPersist */
+    //public function postPersistHandler(User $user, LifecycleEventArgs $event);
+
+    /** @ORM\PreUpdate */
+    //public function preUpdateHandler(User $user, PreUpdateEventArgs $event);
+
+    /** @ORM\PostUpdate */
+    //public function postUpdateHandler(User $user, LifecycleEventArgs $event);
+
+    /** @ORM\PostRemove */
+    //public function postRemoveHandler(User $user, LifecycleEventArgs $event);
+
+    /** @ORM\PreRemove */
+    //public function preRemoveHandler(User $user, LifecycleEventArgs $event);
+
+    /** @ORM\PreFlush */
+    //public function preFlushHandler(User $user, PreFlushEventArgs $event);
+
+    /** @ORM\PostLoad */
+    //public function postLoadHandler(User $user, LifecycleEventArgs $event);
 }
