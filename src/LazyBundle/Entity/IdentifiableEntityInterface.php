70c2cf15487564a7d6df5ee2aa6d0d18c794c5c6
GT <788449+gtgt@users.noreply.github.com>
2021-01-19 22:46:23 +0100

    - phpdoc fix

--- src/LazyBundle/Entity/IdentifiableEntityInterface.php
+++ src/LazyBundle/Entity/IdentifiableEntityInterface.php
@@ -1,11 +1,16 @@
 <?php
 namespace LazyBundle\Entity;
 
+use phpDocumentor\Reflection\Types\Scalar;
+
 /**
  * Interface for entities with (unique) identifier requirement
  *
  * @package LazyBundle\Entity
  */
 interface IdentifiableEntityInterface extends EntityInterface {
+    /**
+     * @return string|int|float|null
+     */
     public function getId();
 }
