01beb97209cf76d85b1547268af24e8dbeaf406a
GT <788449+gtgt@users.noreply.github.com>
2021-06-08 15:46:49 +0200

    Cron DataCollector improvement

--- src/LazyBundle/DataCollector/CronDataCollector.php
+++ src/LazyBundle/DataCollector/CronDataCollector.php
@@ -2,6 +2,7 @@
 
 namespace LazyBundle\DataCollector;
 
+use Jobby\Jobby;
 use Symfony\Bundle\FrameworkBundle\DataCollector\AbstractDataCollector;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,23 +11,24 @@ class CronDataCollector extends AbstractDataCollector {
     /**
      * @var array
      */
-    private $config;
+    private $jobby;
 
     /**
      * @param array $cronConfig
      */
-    public function __construct(array $cronConfig) {
-        $this->config = $cronConfig;
+    public function __construct(Jobby $jobby) {
+        $this->jobby = $jobby;
     }
 
     /**
      * @inheritDoc
      */
     public function collect(Request $request, Response $response, \Throwable $exception = null) {
+        $jobs = $this->jobby->getJobs();
         $this->data = [
             'jobs' => array_map(function(string $name, array $config) {
                 return [$name, $config['command'], $config['schedule'], $config['enabled'] ? 'true' : 'false',];
-            }, array_keys($this->config['jobs']), $this->config['jobs']),
+            }, array_column($jobs, 0), array_column($jobs, 1)),
             'columns' => ['name', 'command', 'schedule', 'enabled'],
         ];
     }
@@ -49,5 +51,9 @@ class CronDataCollector extends AbstractDataCollector {
 
     public static function getTemplate(): ?string {
         return '@Lazy/Collector/cron.html.twig';
+    }
+
+    public function __sleep() {
+        return ['data'];
     }
 }
